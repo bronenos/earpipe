f56b6a201daefebac38d1d20fc87417d0a8df079
Stan Potemkin <bronenos@gmail.com>
2013-08-18 22:42:00 +0400

    Initial Commit

---
+++ EarPipe/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  EarPipe
+//
+//  Created by Stan Potemkin on 8/18/13.
+//  Copyright (c) 2013 Stan Potemkin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ EarPipe/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  EarPipe
+//
+//  Created by Stan Potemkin on 8/18/13.
+//  Copyright (c) 2013 Stan Potemkin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
