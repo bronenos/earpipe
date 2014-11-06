61ebc2360066f1ba5dd0adc29f938746fba20c73
Stan Potemkin <bronenos@gmail.com>
2014-11-06 08:14:12 +0300

    master: (1) just improve for baby listener

--- EarPipe/EPRouteController.m
+++ EarPipe/EPRouteController.m
@@ -7,13 +7,14 @@
 //
 
 #import <AVFoundation/AVAudioSession.h>
+#import <AudioToolbox/AudioToolbox.h>
 #import "EPRouteController.h"
 
 
 NSString * const EPRouteControllerDeviceDiscovered = @"EPRouteControllerDeviceDiscovered";
 
 
-@interface EPRouteController()
+@interface EPRouteController() <GKVoiceChatClient>
 @property(nonatomic, strong) GKSession *gamekitSession;
 @property(nonatomic, strong) CBPeripheralManager *bluetoothManager;
 
@@ -102,14 +103,42 @@ NSString * const EPRouteControllerDeviceDiscovered = @"EPRouteControllerDeviceDi
 		return;
 	}
 	
-	NSData *data = [@"Hello" dataUsingEncoding:NSUTF8StringEncoding];
-	[self.gamekitSession sendDataToAllPeers:data withDataMode:GKSendDataReliable error:nil];
+	AVAudioSession *audioSession = [AVAudioSession sharedInstance];
+	NSError *error;
+	
+	if (![audioSession setCategory:AVAudioSessionCategoryPlayAndRecord error:&error])
+	{
+		NSLog(@"Error setting the AV play/record category: %@", [error localizedDescription]);
+//		showAlert(@"Could not establish an Audio Connection. Sorry!");
+		return;
+	}
+	
+	if (![audioSession setActive: YES error: &error])
+	{
+		NSLog(@"Error activating the audio session: %@", [error localizedDescription]);
+//		showAlert(@"Could not establish an Audio Connection. Sorry!");
+		return;
+	
+	
+	}
+	
+	GKVoiceChatService *chatService = [GKVoiceChatService defaultVoiceChatService];
+	if (chatService) {
+		NSArray *peers = [self.gamekitSession peersWithConnectionState:GKPeerStateConnected];
+		
+		chatService.client = self;
+		[chatService startVoiceChatWithParticipantID:[peers firstObject] error:nil];
+		[GKVoiceChatService defaultVoiceChatService].remoteParticipantVolume = 0;
+	}
 }
 
 
 #pragma mark - Internal
 - (void)GK_startScanning
 {
+	[GKVoiceChatService defaultVoiceChatService].client = self;
+	
+	
 	GKPeerPickerController *picker = [[GKPeerPickerController alloc] init];
 	picker.delegate = self;
 	picker.connectionTypesMask = GKPeerPickerConnectionTypeNearby;
@@ -168,7 +197,18 @@ NSString * const EPRouteControllerDeviceDiscovered = @"EPRouteControllerDeviceDi
 
 - (void)receiveData:(NSData *)data fromPeer:(NSString *)peer inSession:(GKSession *)session context:(void *)context
 {
-	// ...
+	char t = ((char*)data.bytes)[0];
+	NSData *d = [data subdataWithRange:NSMakeRange(1, data.length - 1)];
+	
+	switch (t) {
+		case 0x00:
+			[[GKVoiceChatService defaultVoiceChatService] receivedData:d fromParticipantID:peer];
+			break;
+			
+		case 0x01:
+			[[GKVoiceChatService defaultVoiceChatService] receivedRealTimeData:d fromParticipantID:peer];
+			break;
+	}
 }
 
 
@@ -193,6 +233,49 @@ NSString * const EPRouteControllerDeviceDiscovered = @"EPRouteControllerDeviceDi
 - (void)peripheral:(CBPeripheral *)peripheral didDiscoverServices:(NSError *)error
 {
 	NSLog(@"%s -> %@", __FUNCTION__, error);
+}
+
+
+
+- (NSString *)participantID
+{
+	return self.gamekitSession.peerID;
+}
+
+
+- (void)voiceChatService:(GKVoiceChatService *)voiceChatService didReceiveInvitationFromParticipantID:(NSString *)participantID callID:(NSInteger)callID
+{
+	UInt32 audioRouteOverride = kAudioSessionOverrideAudioRoute_Speaker;
+	
+	AudioSessionSetProperty (
+							 kAudioSessionProperty_OverrideAudioRoute,
+							 sizeof (audioRouteOverride),
+							 &audioRouteOverride
+							 )
+	
+	;	[[GKVoiceChatService defaultVoiceChatService] acceptCallID:callID error:nil];
+	[GKVoiceChatService defaultVoiceChatService].remoteParticipantVolume = 1.0;
+}
+
+
+- (void)voiceChatService:(GKVoiceChatService *)voiceChatService sendData:(NSData *)data toParticipantID:(NSString *)participantID
+{
+	char b = 0x00;
+	
+	NSMutableData *d = [NSMutableData data];
+	[d appendBytes:&b length:sizeof(b)];
+	[d appendData:data];
+	[self.gamekitSession sendData:d toPeers:@[participantID] withDataMode:GKSendDataReliable error:nil];
+}
+
+- (void)voiceChatService:(GKVoiceChatService *)voiceChatService sendRealTimeData:(NSData *)data toParticipantID:(NSString *)participantID
+{
+	char b = 0x01;
+	
+	NSMutableData *d = [NSMutableData data];
+	[d appendBytes:&b length:sizeof(b)];
+	[d appendData:data];
+	[self.gamekitSession sendData:d toPeers:@[participantID] withDataMode:GKSendDataUnreliable error:nil];
 }
 
 @end
